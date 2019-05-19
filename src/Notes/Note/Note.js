84d6d505356f5080c9a06a2ee33a9c28c7cdf3f2
mgwedd <michael.wedd@gmail.com>
2019-05-19 18:11:22 -0400

    finished Note component

--- src/Notes/Note/Note.js
+++ src/Notes/Note/Note.js
@@ -4,23 +4,40 @@ import { format } from 'date-fns'
 
 export default function Note( props ) {
     const { note, path } = props;
-    
-    // if path is not a specific note, render the note overview view.
-    if (path !== '/note') {
-        return (
-            <>
-                <div className="note_container">
-                    <Link>
+    const standardNote = (
+            <div className="note_container">
+                <div className="note_title-and-date-wrapper">
+                    <Link
+                        to={`Note/${note.id}`}>
                         <h2 className="note_title"> 
                             { note.title }
                         </h2>
                     </Link>
-                    <span className="note_modified"></span>
+                    <span className="note_modified"> 
+                        Modified 
+                        {' '}
+                        {format(note.modified, 'Do MMM YYYY')} 
+                    </span>
+                </div>
+                <div classname="note_delete-button-wrapper">
+                    <button className="note_delete-button">
+                        Delete
+                    </button>
+                </div>
+            </div>
+    );  
+    // if the path isn't targeting a specific note, render a standard note overview; 
+    // otherwise, render that overview with a content box below it
+    if (path !== `/note/${note.id}`) 
+        return { standardNote };
+    else {     
+        return (
+            <>
+                { standardNote }
+                <div className="note_content">
+                    { note.content }
                 </div>
             </>
         );  
-    } else {
-
     }
-    // if path is /note/:noteId, then render the full note content etc. 
 }
--- src/Notes/NoteList/NoteList.js
+++ src/Notes/NoteList/NoteList.js
@@ -1,3 +1,14 @@
+import React from 'react';
+import Note from '../Note/Note';
+import { findNote, getNotesForFolder }from '../../helper-functions';
 // gets a list of ids for notes that should be rendered. 
 // passes each id to Note, which provides a component based on that noteId.
 
+export default function NoteList( props ) {
+    
+    return (
+        <>
+
+        </>
+    );
+}
