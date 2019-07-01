32186da1646f910f87e501386715cdf989067e53
mgwedd <michael.wedd@leverege.com>
2019-07-01 09:03:16 -0400

    got fetch working, but still returning promise object...

--- src/App.js
+++ src/App.js
@@ -7,6 +7,47 @@ import Main from './Main/Main';
 import config from './config';
 import './App.css';
 
+async function apiInterface( method = 'GET', endpoint = 'note', resourceId = false, body = false ) {
+  let requestEndpoint = `${config.API_BASE}/${endpoint}`
+  
+  if ( resourceId ) {
+    requestEndpoint += `/${resourceId}`
+  }
+
+  const request = {
+    method,
+    headers: {
+      'content-type': 'application/json',
+      'Authorization': `Bearer ${config.API_TOKEN}`
+    }
+  }
+
+  if ( body ) {
+    request.body = JSON.stringify( body )
+  }
+
+  try {
+    return await fetch( requestEndpoint, request )
+      .then( res => {
+        if ( !res.ok ) {
+          return res
+            .json()
+            .then( error => {
+              throw error
+            })
+        }
+        return res.json()
+      })
+      .then( resolvedResponse => {
+        console.log('about to resolve with: ', resolvedResponse)
+        return resolvedResponse
+      })
+  }
+  catch( err ) {
+    return `API Interface Error: ${err.message}`
+  }
+}
+
 export default class App extends Component {
 
   state = {
@@ -37,28 +78,23 @@ export default class App extends Component {
       ]
     });
   }
-
+ 
   componentDidMount() {
-    Promise.all([
-      fetch(`${ config.API_ENDPOINT }/notes`),
-      fetch(`${ config.API_ENDPOINT }/folders`)
-    ])
-    .then(([notesRes, foldersRes]) => {
-      if (!notesRes.ok)
-        return notesRes.json().then(badNotesResponse => Promise.reject(badNotesResponse));
-      if (!foldersRes.ok)
-        return foldersRes.json().then(badFoldersResponse => Promise.reject(badFoldersResponse));
-        return Promise.all([
-          notesRes.json(),
-          foldersRes.json(),
-        ]);
-      })
-      .then(([notes, folders]) => {
-        this.setState({ notes, folders });
-      })
-      .catch(error => {
-        console.error({ error });
-      });
+    const notes = apiInterface('GET', 'note')
+    console.log('component mounted with these new note results: ', notes)
+    // const folders = apiInterface('GET', 'folder')
+
+    if (typeof notes !== 'string') {
+      this.setState( {notes : notes} )
+    } else {
+      console.log(`ERROR ON INITIAL FETCH: ${notes}`)
+    }
+
+    // if (typeof folders !== 'string') {
+    //   this.setState( {...folders} )
+    // } else {
+    //   console.log(`ERROR ON INITIAL FETCH: ${folders}`)
+    // }
   }
 
   render() {
@@ -73,7 +109,7 @@ export default class App extends Component {
     return (
       <>
         <Header />
-        <NotefulContext.Provider value={ contextValue }>
+        <NotefulContext.Provider value={contextValue}>
           <div className="sidebar_and_main_container">
             <div className="sidebar_container">
             <NotefulErrorBoundary>
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-
 export default {
-    API_ENDPOINT: 'http://localhost:9090'
+    API_BASE : 'http://localhost:8000/api', 
+    API_TOKEN : '1db06954-6279-447e-89aa-4dd7173ae4d4'
 }
